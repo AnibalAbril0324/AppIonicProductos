7c064d608b7d1e9de7eb6776f7c8386208b218c7
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-04-08 16:32:13 -0500

    Desarrollo del Perfil

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/models/Product.models';
+import { User } from 'src/app/models/user.models';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  firebaseService =inject(FirebaseService);
+  utilsSvc=inject(UtilsService)
+
   constructor() { }
 
   ngOnInit() {
   }
+  user(): User{
+    return this.utilsSvc.getFromLocalStorage('user');
+  }
 
+  async takeImage(){
+    let user=this.user();
+    let path= `users/${user.uid}`
+      
+    const dataUrl= (await this.utilsSvc.takePicture('Imagen del Perfil')).dataUrl;
+
+    const loading=await this.utilsSvc.loading();
+    await loading.present();
+
+    let imagePath= `${user.uid}/profile`;
+    user.image = await this.firebaseService.uploadImage(imagePath,dataUrl); 
+
+    this.firebaseService.updateDocument(path,{image: user.image}).then( async res => {
+        
+      this.utilsSvc.saveInLocalStorage('user',user);
+
+      this.utilsSvc.presentToast({
+        message: 'Imagen Actualizada Exitosamente',
+        duration: 1500,
+        color:'success',
+        position:'middle',
+        icon: 'checkmark-circle-outline'
+      });
+
+    }).catch(error => {
+      console.log(error);
+      this.utilsSvc.presentToast({
+        message: 'ERROR  EL CORREO O CONTRASEÑA SON INCORRECTOS',
+        duration: 2500,
+        color:'primary',
+        position:'middle',
+        icon: 'alert-circle'
+      });
+
+    }).finally( () => {
+      loading.dismiss();
+    })
+  }
 }
